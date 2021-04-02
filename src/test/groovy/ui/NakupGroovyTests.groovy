50ab437fd639ef463dab9a4d52c73bbf4494fd64
st52542 <43936282+st52542@users.noreply.github.com>
2021-04-02 03:09:04 +0200

    ui tests complete

--- src/test/groovy/ui/NakupGroovyTests.groovy
+++ src/test/groovy/ui/NakupGroovyTests.groovy
@@ -3,6 +3,7 @@ package ui
 import cz.vlastni.eshop.EshopApplication
 import cz.vlastni.eshop.dataFactory.Creator
 import cz.vlastni.eshop.entity.Doprava
+import cz.vlastni.eshop.entity.Nakup
 import cz.vlastni.eshop.entity.Platba
 import cz.vlastni.eshop.entity.Uzivatel
 import cz.vlastni.eshop.repository.NakupRepository
@@ -51,77 +52,82 @@ class NakupGroovyTests {
     }
 
     @Test
-    public void userGetToAddNewUzivatel() {
-        driver.get("http://localhost:8080");
-        driver.findElement(By.xpath("//a[@href='/checkout']")).click();
+    public void userGetToAddNewNakup() {
+        driver.get("http://localhost:8080/");
+        driver.findElement(By.xpath("//a[@href='checkout']")).click();
         Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Konecna Hlaska']")).size());
     }
 
     @Test
-    public void userSaveNewUzivatel() {
+    public void userSaveFirstValueNewNakup() {
         creator.save(new Uzivatel(jmeno: "jmeno1"))
         creator.save(new Doprava(popis: "doprava1"));
         creator.save(new Platba(popis: "platba1"));
         driver.get("http://localhost:8080/checkout");
-        driver.findElement(By.name("jmeno")).sendKeys("jmeno1");
-        driver.findElement(By.name("prijmeni")).sendKeys("prijmeni1");
-        driver.findElement(By.name("heslo")).sendKeys("heslo1");
-        driver.findElement(By.name("adresa")).sendKeys("adresa1");
-        driver.findElement(By.name("email")).sendKeys("email@email.cz");
-        driver.findElement(By.name("admin")).sendKeys("true");
         driver.findElement(By.xpath("//input[@type='submit']")).click();
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Seznam uzivatelu']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='jmeno1']")).size());
+        List<Nakup> nakupy=nakupRepository.findAll();
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Seznam nakupu']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='"+nakupy.get(0).getObjednavka()+"']")).size());
+        driver.findElement(By.xpath("//h4[text()='"+nakupy.get(0).getObjednavka()+"']")).click();
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='platba1']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='doprava1']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='jmeno1']")).size());
     }
 
     @Test
-    public void checkMoreUzivatel() {
+    public void userSaveSecondValueNewNakup() {
+        creator.save(new Uzivatel(jmeno: "jmeno1"))
+        creator.save(new Uzivatel(jmeno: "jmeno2"))
+        creator.save(new Doprava(popis: "doprava1"));
+        creator.save(new Doprava(popis: "doprava2"));
+        creator.save(new Platba(popis: "platba1"));
+        creator.save(new Platba(popis: "platba2"));
+        driver.get("http://localhost:8080/checkout");
+        driver.findElement(By.xpath("//select[@name='platba']/option[2]")).click();
+        driver.findElement(By.xpath("//select[@name='doprava']/option[2]")).click();
+        driver.findElement(By.xpath("//select[@name='uzivatel']/option[2]")).click();
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+        List<Nakup> nakupy=nakupRepository.findAll();
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Seznam nakupu']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='"+nakupy.get(0).getObjednavka()+"']")).size());
+        driver.findElement(By.xpath("//h4[text()='"+nakupy.get(0).getObjednavka()+"']")).click();
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='platba2']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='doprava2']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='jmeno2']")).size());
+    }
+
+    @Test
+    public void checkMoreNakupu() {
         creator.saveEntities(
-                new Uzivatel(jmeno: "jmeno1"),
-                new Uzivatel(jmeno: "jmeno2"),
-                new Uzivatel(jmeno: "jmeno3"),
-                new Uzivatel(jmeno: "jmeno4")
+                new Nakup(objednavka: 111111),
+                new Nakup(objednavka: 222222),
+                new Nakup(objednavka: 333333),
+                new Nakup(objednavka: 444444)
         )
-        driver.get("http://localhost:8080/uzivatel");
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Seznam uzivatelu']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='jmeno1']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='jmeno2']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='jmeno3']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='jmeno4']")).size());
+        driver.get("http://localhost:8080/");
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Seznam nakupu']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='111111']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='222222']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='333333']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='444444']")).size());
     }
     @Test
     public void userCheckValidDetails() {
         creator.saveEntities(
-                new Uzivatel(jmeno: "jmeno1", prijmeni: "prijmeni1"),
-                new Uzivatel(jmeno: "jmeno2", prijmeni: "prijmeni2"),
-                new Uzivatel(jmeno: "jmeno3", prijmeni: "prijmeni3"),
-                new Uzivatel(jmeno: "jmeno4", prijmeni: "prijmeni4")
+                new Nakup(objednavka: 111111),
+                new Nakup(objednavka: 222222),
+                new Nakup(objednavka: 333333),
+                new Nakup(objednavka: 444444)
         )
-        driver.get("http://localhost:8080/uzivatel");
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Seznam uzivatelu']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='jmeno1']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='jmeno2']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='jmeno3']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='jmeno4']")).size());
-        driver.findElement(By.xpath("//h4[text()='jmeno1']")).click();
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Detail uzivatele']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='jmeno1']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='prijmeni1']")).size());
-    }
-    @Test
-    public void userEditDetails() {
-        creator.save(new Uzivatel(jmeno: "jmeno1", prijmeni: "prijmeni1"));
-        driver.get("http://localhost:8080/uzivatel-reg-form/1");
-        driver.findElement(By.name("jmeno")).clear();
-        driver.findElement(By.name("jmeno")).sendKeys("jmenoZmena");
-        driver.findElement(By.name("prijmeni")).clear();
-        driver.findElement(By.name("prijmeni")).sendKeys("prijmeniZmena");
-        driver.findElement(By.xpath("//input[@type='submit']")).click();
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Seznam uzivatelu']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='jmenoZmena']")).size());
-        driver.findElement(By.xpath("//h4[text()='jmenoZmena']")).click();
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Detail uzivatele']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='jmenoZmena']")).size());
-        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='prijmeniZmena']")).size());
+        driver.get("http://localhost:8080/");
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Seznam nakupu']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='111111']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='222222']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='333333']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h4[text()='444444']")).size());
+        driver.findElement(By.xpath("//h4[text()='222222']")).click();
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Detail nakupu']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//h3[text()='datum']")).size());
+        Assertions.assertEquals(1, driver.findElements(By.xpath("//p[text()='"+nakupRepository.findByObjednavka(222222).getDatumVytvoreni()+"']")).size());
     }
 }
